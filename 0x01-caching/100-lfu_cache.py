0ba072f19ae58946a204ad41970382d983f3898c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-03 02:01:10 +0000

    refactored

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
-"""Module conatining class with a LFU(Least Frequently Used) caching system"""
-#!/usr/bin/env python3
-"""Module containing class with a LFU(Least Frequently Used) caching system"""
-BaseCaching = __import__('base_caching').BaseCaching
+"""Module for implementing LFUCaching"""
+from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """Class defining methods that assign to dict & return data from it"""
+    """Class to implement LFU caching system"""
 
     def __init__(self):
-        """Function to initialize class instances"""
+        """Initialize class instance."""
 
         super().__init__()
-        self.cache_order = []  # To maintain the order of keys based on frequency
-        self.key_frequency = {}  # To store the frequency of each key
+        self.cache_order = []
+        self.key_frequency = {}
 
     def put(self, key, item):
-        """Function that assigns data to dictionary (cache)"""
+        """Add key/value pair to cache_data. If the number of items in"""
 
         if key and item:
             current_size = len(self.cache_data)
@@ -58,39 +56,40 @@ class LFUCache(BaseCaching):
             return self.cache_data[key]
         return None
 
-my_cache = LFUCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-print(my_cache.get("B"))
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-print(my_cache.get("I"))
-print(my_cache.get("H"))
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
-my_cache.put("L", "L")
-my_cache.print_cache()
-my_cache.put("M", "M")
-my_cache.print_cache()
+
+# my_cache = LFUCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# print(my_cache.get("B"))
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# print(my_cache.get("A"))
+# print(my_cache.get("B"))
+# print(my_cache.get("C"))
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
+# my_cache.put("G", "San Francisco")
+# my_cache.print_cache()
+# my_cache.put("H", "H")
+# my_cache.print_cache()
+# my_cache.put("I", "I")
+# my_cache.print_cache()
+# print(my_cache.get("I"))
+# print(my_cache.get("H"))
+# print(my_cache.get("I"))
+# print(my_cache.get("H"))
+# print(my_cache.get("I"))
+# print(my_cache.get("H"))
+# my_cache.put("J", "J")
+# my_cache.print_cache()
+# my_cache.put("K", "K")
+# my_cache.print_cache()
+# my_cache.put("L", "L")
+# my_cache.print_cache()
+# my_cache.put("M", "M")
+# my_cache.print_cache()
