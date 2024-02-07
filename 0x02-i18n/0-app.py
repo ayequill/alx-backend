1bfd63543f88581fca2632e5bfec769268634016
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-07 12:51:24 +0000

    refactored

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Module for basic flask app """
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,3 @@ app = Flask(__name__)
 def index():
     """ GET request to / """
     return render_template('0-index.html')
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
