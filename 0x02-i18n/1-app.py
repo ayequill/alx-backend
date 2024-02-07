2ae2545d83786c5a4ab8f05dd1817a6022f62c9c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-07 13:04:37 +0000

    added bable wrapped app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module for basic flask app """
+from flask import Flask, render_template
+from flask_babel import Babel
+from babel_config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', )
+def index():
+    """ GET request to / """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
