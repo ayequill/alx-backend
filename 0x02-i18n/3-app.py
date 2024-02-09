735e6f91a2c53c2698743f1f7c8b49ebb896b66d
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-02-09 21:34:50 +0000

    Parametrize templates and use msg ids

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module for basic flask app """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 from babel_config import Config
 
 app = Flask(__name__)
@@ -18,9 +18,4 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """ GET request to / """
-    return render_template('3-index.html',
-                           title=gettext('home_title'), header=gettext('home_header'))
-
-
-if __name__ == "__main__":
-    app.run()
+    return render_template('3-index.html')
